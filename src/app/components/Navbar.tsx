acdb8cea11512addca9a13353664cb6cc5044a29
mylanlegenie <mylanhachemane@gmail.com>
2025-06-06 19:15:04 +0200

    Autre probleme devis

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar() {
                 {/* Desktop menu */}
                 <ul className="hidden md:flex gap-8 text-gray-700 font-medium z-0">
                     <li><RubriquesDesktop lien="/qui-sommes-nous" label="Qui sommes nous ?"></RubriquesDesktop></li>
-                    <li><RubriquesDesktop lien="/nous-contacter" label="Nous Contacter"></RubriquesDesktop></li>
+                    <li><RubriquesDesktop lien="/devis" label="Nous Contacter"></RubriquesDesktop></li>
                 </ul>
 
                 {/* Burger / Croix mobile */}
@@ -64,7 +64,7 @@ export default function Navbar() {
             {menuOpen && (
                 <ul className="absolute top-full left-0 w-full bg-white shadow-md flex flex-col items-center py-4 space-y-4 md:hidden z-0">
                     <li><RubriquesMobile lien="/qui-sommes-nous" label="Qui sommes nous" onClick={() => setMenuOpen(false)} ></RubriquesMobile></li>
-                    <li><RubriquesMobile lien="/nous-contacter" label="Nous Contactez" onClick={() => setMenuOpen(false)} ></RubriquesMobile></li>
+                    <li><RubriquesMobile lien="/devis" label="Nous Contactez" onClick={() => setMenuOpen(false)} ></RubriquesMobile></li>
                 </ul>
             )}
         </nav>
