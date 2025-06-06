0a09dc08252be104b22cce18ec242a1948465c3a
mylanlegenie <mylanhachemane@gmail.com>
2025-06-06 18:13:32 +0200

    Wrong Link Delete

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -45,9 +45,7 @@ export default function Navbar() {
                 {/* Desktop menu */}
                 <ul className="hidden md:flex gap-8 text-gray-700 font-medium z-0">
                     <li><RubriquesDesktop lien="/qui-sommes-nous" label="Qui sommes nous ?"></RubriquesDesktop></li>
-                    <li><RubriquesDesktop lien="/nos-tarifs" label="Nos Tarifs"></RubriquesDesktop></li>
                     <li><RubriquesDesktop lien="/nous-contacter" label="Nous Contacter"></RubriquesDesktop></li>
-                    <li><RubriquesDesktop lien="" label="I don't know"></RubriquesDesktop></li>
                 </ul>
 
                 {/* Burger / Croix mobile */}
--- src/app/components/NosTarifs.tsx
+++
@@ -1,7 +0,0 @@
-export default function NosTarifs() {
-    return (
-        <div>
-            <h1>Nos Tarifs</h1>
-        </div>
-    )
-}
