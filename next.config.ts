ac87a7047c7af02ba70d5abc0eb721bb3e3d7838
mylanlegenie <mylanhachemane@gmail.com>
2025-06-12 23:51:56 +0200

    Security Headers

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,29 @@ import type { Configuration } from 'webpack';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self';"
+          }
+        ],
+      },
+    ];
+  },
+
   webpack(config: Configuration) {
     config.module?.rules?.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/, // fichiers .ts, .tsx, .js, .jsx
+      issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
 
